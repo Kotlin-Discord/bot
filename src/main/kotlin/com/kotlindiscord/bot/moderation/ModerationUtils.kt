bfe2b8f06aeeaba894fa73bb988d7bbc9f3b351d
Matteo Bertucci <matteobertucci2004@gmail.com>
2020-07-02 12:12:47 +0200

    Create createInfraction and upsertInfraction utils commands

---
+++ src/main/kotlin/com/kotlindiscord/bot/moderation/ModerationUtils.kt
@@ -0,0 +1,25 @@
+package com.kotlindiscord.bot.moderation
+
+import com.gitlab.kordlib.core.entity.Message
+import com.gitlab.kordlib.core.entity.User
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import kotlin.reflect.full.primaryConstructor
+
+suspend inline fun <reified T: InfractionType> createInfraction(message: Message, infractor: User, reason: String, expires: LocalDateTime): T {
+    val data = InfractionData(
+        message.getGuild(),
+        infractor,
+        message.author!!,
+        reason,
+        expires,
+        LocalDateTime.ofInstant(message.timestamp, ZoneOffset.UTC)
+    )
+
+    val ctor = T::class.primaryConstructor ?: throw IllegalArgumentException("${T::class.simpleName} has no primary constructor.")
+    return ctor.call(data)
+}
+
+suspend fun upsertInfraction(infraction: InfractionType) {
+    infraction.upsert()
+}
