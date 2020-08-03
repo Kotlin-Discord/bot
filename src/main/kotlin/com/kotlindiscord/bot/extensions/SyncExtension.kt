9a30d2c0a490cb117adcadedf2efcc9b5982a7e4
Gareth Coles <gdude2002@gmail.com>
2020-08-03 14:15:33 +0100

    Initial user and role sync seem to work now

--- src/main/kotlin/com/kotlindiscord/bot/extensions/SyncExtension.kt
+++ src/main/kotlin/com/kotlindiscord/bot/extensions/SyncExtension.kt
@@ -4,12 +4,15 @@ import com.gitlab.kordlib.core.behavior.channel.createEmbed
 import com.gitlab.kordlib.core.entity.channel.TextChannel
 import com.gitlab.kordlib.core.event.gateway.ReadyEvent
 import com.kotlindiscord.api.client.models.RoleModel
+import com.kotlindiscord.api.client.models.UserModel
 import com.kotlindiscord.bot.config.config
 import com.kotlindiscord.bot.enums.Channels
 import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.events.EventHandler
 import com.kotlindiscord.kord.extensions.extensions.Extension
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.flow.toSet
 import mu.KotlinLogging
 
 /**
@@ -32,6 +35,7 @@ class SyncExtension(bot: ExtensibleBot) : Extension(bot) {
     @Suppress("UnusedPrivateMember")  // Odd way to point out unused function params, isn't it
     suspend fun initialSync(handler: EventHandler<ReadyEvent>, event: ReadyEvent) {
         val (rolesUpdated, rolesRemoved) = updateRoles()
+        val (usersUpdated, usersScrubbed) = updateUsers()
 
         (config.getChannel(Channels.ALERTS) as TextChannel)
             .createEmbed {
@@ -39,6 +43,9 @@ class SyncExtension(bot: ExtensibleBot) : Extension(bot) {
                 description = """
                     **Roles updated:** $rolesUpdated
                     **Roles removed:** $rolesRemoved
+                    
+                    **Users updated:** $usersUpdated
+                    **Users scrubbed:** $usersScrubbed
                 """.trimIndent()
             }
     }
@@ -77,5 +84,56 @@ class SyncExtension(bot: ExtensibleBot) : Extension(bot) {
         rolesToRemove.forEach { config.api.deleteRole(it) }
 
         return Pair(rolesToUpdate.size, rolesToRemove.size)
+    }
+
+    /**
+     * @suppress
+     */
+    suspend fun updateUsers(): Pair<Int, Int> {
+        val dbUsers = config.api.getUsers().map { it.id to it }.toMap()
+        val discordUsers = config.getGuild().members.toList().map { it.id.longValue to it }.toMap()
+        val usersToUpdate = mutableListOf<UserModel>()
+        val usersToScrub = mutableListOf<UserModel>()
+
+        for ((id, user) in discordUsers.entries) {
+            val dbUser = dbUsers[id]
+
+            if (dbUser == null) {
+                usersToUpdate.add(
+                    UserModel(
+                        id, user.username, user.discriminator, user.avatar.url,
+                        user.roles.map { it.id.longValue }.toSet()
+                    )
+                )
+            } else if (  // Check if there's anything to update
+                dbUser.username != user.username ||
+                dbUser.discriminator != user.discriminator ||
+                dbUser.avatarUrl != user.avatar.url ||
+                dbUser.roles != user.roles.map { it.id.longValue }.toSet()
+            ) {
+                usersToUpdate.add(
+                    UserModel(
+                        id, user.username, user.discriminator, user.avatar.url,
+                        user.roles.map { it.id.longValue }.toSet()
+                    )
+                )
+            }
+        }
+
+        for ((id, user) in dbUsers.entries) {
+            if (discordUsers[id] == null && user.present) {
+                usersToScrub.add(  // TODO: Think about mutability in API models?
+                    UserModel(
+                        user.id, user.username, user.discriminator,
+                        user.avatarUrl, user.roles, false
+                    )
+                )
+            }
+        }
+
+        usersToUpdate.forEach { config.api.upsertUser(it) }
+        usersToScrub.forEach { config.api.upsertUser(it) }
+
+        return Pair(usersToUpdate.size, usersToScrub.size)
     }
 }
